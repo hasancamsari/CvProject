23bcc6538dafab1901906ef5194cf1d6ccad64f3
hasancamsari <hasancmsari@gmail.com>
2025-05-21 19:05:24 +0300

    Add project files.

---
+++ CvProject/Controllers/AdminDefaultController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CvProject.Models;
+using CvProject.Models.Entity;
+
+namespace CvProject.Controllers
+{
+    public class AdminDefaultController : Controller
+    {
+        // GET: AdminDefault
+        CvProjectEntities3 db = new CvProjectEntities3();
+        public ActionResult Index()
+        {
+            var model = new AllData
+            {
+                HomeData = db.TBLMAIN.ToList(),
+                AboutData = db.TBLABOUT.ToList(),
+                SkillsData = db.TBLSKILLS.ToList(),
+                WorksData = db.TBLPROJECTS.ToList(),
+                ContactData = db.TBLCONTACT.ToList()
+            };
+            return View(model);
+        }
+
+        public ActionResult HeaderP()
+        {
+            return View();
+        }
+        public ActionResult SidebarP()
+        {
+            return View();
+        }
+        public ActionResult NavbarP()
+        {
+            return View();
+        }
+
+        public ActionResult FooterP()
+        {
+            return View();
+        }
+
+        public ActionResult ScriptsP()
+        {
+            return View();
+        }
+    }
+}
---
+++ CvProject/Models/Dto/AllData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CvProject.Models.Entity;
+
+namespace CvProject.Models
+{
+    public class AllData
+    {
+        public List<TBLMAIN> HomeData { get; set; }
+        public List<TBLABOUT> AboutData { get; set; }
+        public List<TBLSKILLS> SkillsData { get; set; }
+        public List<TBLPROJECTS> WorksData { get; set; }
+        public List<TBLCONTACT> ContactData { get; set; }
+    }
+}
